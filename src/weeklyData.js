84a20794279fe133752be33b1b4adace161968b4
Giang <tgn1992@hotmail.de>
2019-04-22 23:00:19 +0200

    Finishing weeklyData

--- src/weeklyData.js
+++ src/weeklyData.js
@@ -1,50 +1,50 @@
 export const weeklyData = [
     {
         logo: 'fas fa-leaf',
-        title: 'Nature Awareness',
-        text: 'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        title: 'Environment',
+        text: 'The world is the only home we have and it\'s time that every single one of us takes responsibility for the environment. We all can contribute to a better place for you, me and the entire human race.',
         first: 'Think about how you can reduce your CO² footprint.',
         second: 'Use as little plastic as possible this week.'
     },
     {
         logo: 'fas fa-spa',
         title: 'Mindfullness',
-        text: 'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
-        first: ' Try to just focus at one thing if you do something!',
+        text: 'If we try to be fully present in the moment, you will notice that most of our problems lie either in the past or in the future. Being mindfull helps you to focus on what is really important and that is only the present.',
+        first: ' Try to just focus at one thing at a time!',
         second: 'Reduce the amount of media consumption.'
     },
     {
         logo: 'fas fa-utensils',
-        title: 'Nutrition',
-        text: 'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
+        title: 'Health',
+        text: 'Our health is the most valuable thing we have. Fuel your body with fresh and nutritious food so it can function the way it should and provide you a lot of energy for your day. Without a healthy body nothing else matters.',
         first: 'Eat less animal products, sugar, unhealthy fat and short-chain carbohydrates. Eat more fruits, vegetables, nuts and whole grain!',
         second: 'Be as strict as possible in your diet!'
     },
     {
         logo: 'fas fa-running',
         title: 'Fitness',
-        text: 'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+        text: 'Our body is designed to move. Unfortunately most of us don\'t get enough exercise in. But not only your body but also your brain and your mind benefits from movement. You will feel fitter and more energized.',
         first: 'Try to move a lot and skip shortcuts such as cars, trains or busses!',
         second: 'Start a new sport this week!'
     },
     {
         logo: 'fas fa-user-plus',
         title: 'Self improvement',
-        text: 'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. sed diam nonumy eirmod tempor',
+        text: 'You never get taught how to live life althought it should be the most important task in life. Investing time in self improvement allows you to get to know yourself and to pursue a meaningfull life.',
         first: 'Take at least 5 minutes a day to think about yourself, your emotions and thoughts. Just feel them and don\'t judge them.',
         second: 'Read a book about self improvement this week.'
     },
     {
         logo: 'fas fa-user-friends',
         title: 'Relationship',
-        text: 'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum',
+        text: 'Studies show that our personal environment effect our mood the most. Don\'t waste time with people that drain energy from you. Better surround yourself with positive people that lift you up. ',
         first: 'Take time for at least one person you have neglected lately.',
         second: 'Think about your social environment and whether you are satisfied with it.'
     },
     {
         logo: 'fas fa-briefcase',
         title: 'Work',
-        text: 'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. sed diam nonumy eirmod tempor invidunt ut labore et dolore magna',
+        text: 'You spend huge part of your life working. Wouldn\'t it be nice to find a work that fullfills you gives your life a purpose. Follow your passion and don\'t just work for the money.',
         first: 'Is your work fulfilling and does it inspire you every day? Think about what you can change.',
         second: 'Be especially dedicated and motivated. Show your talent and no excuses!'
     }
